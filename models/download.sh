042a563864b872bbd917afcc3754af5799a3d364
TroddenSpade <troddenspade@gmail.com>
2023-03-19 20:25:07 +0330

    Create download.sh

---
+++ models/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Download the pre-trained model
+wget https://github.com/ayoolaolafenwa/PixelLib/releases/download/1.1/xception_pascalvoc.pb
+wget https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
