aed4a799d2039d5cd3bb374286c97a3c437c216a
TroddenSpade <troddenspade@gmail.com>
2023-07-04 00:08:33 +0430

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,24 @@ def convert(file_name, output_path, params):
         config = json.load(jsonfile)
 
     img = remove_bg(file_name)
+
     img = sculpt(img)
     lvls = tuple(find_levels(img))
     print(lvls)
 
+    
+    if params["image_scale"] == 'auto':
+        params["image_scale"] = 900 / max(img.shape[0], img.shape[1])
+
+    scale_x = int(img.shape[1] * float(params["image_scale"]))
+    scale_y = int(img.shape[0] * float(params["image_scale"]))
+    img = cv2.resize(img, (scale_x, scale_y), interpolation=cv2.INTER_LINEAR)
+
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    print(img.shape)
     hatch(img, output_path=output_path, circular=False, 
-          image_scale=float(params["image_scale"]), hatch_angle=int(params["hatch_angle"]), board_config=config, 
+          hatch_angle=int(params["hatch_angle"]), board_config=config, 
           hatch_pitch=1.5, levels=lvls, blur_radius=1)
 
 
@@ -27,4 +38,9 @@ if __name__ == "__main__":
     seconds = dt.timestamp()
 
     os.mkdir(f"outputs/{seconds}")
-    convert("Taghirad.jpg", f"outputs/{seconds}")
+
+    params = {
+        "image_scale": 'auto', # or a float number like 0.2
+        "hatch_angle": 45
+    }
+    convert("Taghirad.jpg", f"outputs/{seconds}", params)
